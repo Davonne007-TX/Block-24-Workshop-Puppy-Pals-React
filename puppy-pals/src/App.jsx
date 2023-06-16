4d7eebbc10453c5208cbc6fbb9d055e333f49fff
Vonney <davonnejv@gmail.com>
2023-06-15 23:49:06 -0500

    adding useState

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react' // you have to invoke this,you have to use it 
 import './App.css'
 import {puppyList} from './data.js' //adds /imports puppyList array from data.js
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log([puppies]);
 
   return (
     <>
