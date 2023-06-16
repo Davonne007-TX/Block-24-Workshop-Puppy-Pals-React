385ea53539673ee674de8533c16b0dac4ecb8935
Vonney <davonnejv@gmail.com>
2023-06-16 00:13:41 -0500

    adding puppies

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react' // you have to invoke this,you have to use it 
-import './App.css'
+import { useState } from 'react' // React can intelligently reflect the updated data. To use this feature of a React component, you need to use the useState hook.
 import {puppyList} from './data.js' //adds /imports puppyList array from data.js
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log([puppies]);
+  console.log("PuppyList:", puppyList);
 
   return (
     <>
-      <div>
-        <h1>Hi</h1>
-      
-        
-      </div>
-      
+      <div className="App">
+        { 
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>;
+         })
+       }
+     </div>
     </>
-  )
-}
+        );//from the return 
+}  //last curly for function App()
 
 export default App
