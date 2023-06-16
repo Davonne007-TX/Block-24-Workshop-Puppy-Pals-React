faf3fb964b2b6ea00f850259b98ef43bd7cb080e
Vonney <davonnejv@gmail.com>
2023-06-16 00:41:42 -0500

    onClick show id

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -3,19 +3,21 @@ import {puppyList} from './data.js' //adds /imports puppyList array from data.js
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("PuppyList:", puppyList);
 
   return (
     <>
       <div className="App">
         { 
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
+          puppies.map((puppy)=> {
+            return <p onClick = {()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
          })
        }
+       {featPupId && <p> {featPupId}</p> }
      </div>
     </>
-        );//from the return 
+        );//from the return s
 }  //last curly for function App()
 
 export default App
